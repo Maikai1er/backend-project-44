a78acb6f5261a191fa57486a24fb27a09bbf94cf
Maikai1er <kolesnichenko.misha12@gmail.com>
2022-12-07 23:47:03 +0100

    trying to fix brain-prime

--- games/brain-prime.js
+++ games/brain-prime.js
@@ -3,6 +3,7 @@ import { greeting, gamePattern, getRandomNumber } from '../src/index.js';
 
 const isPrime = () => {
   const number = getRandomNumber(0, 20);
+  console.log(`Question: ${number}`);
   let correctAnswer;
   if (number < 2) {
     correctAnswer = 'no';
@@ -13,9 +14,11 @@ const isPrime = () => {
   for (let i = 2; i < number; i += 1) {
     if (number % i === 0) divisorsCount += 1;
   }
-  if (divisorsCount === 0) correctAnswer = 'yes';
-  else correctAnswer = 'no';
-  console.log(`Question: ${number}`);
+  if (divisorsCount === 0) {
+  } else {
+    correctAnswer = 'yes';
+    correctAnswer = 'no';
+  }
   return correctAnswer;
 };
 
