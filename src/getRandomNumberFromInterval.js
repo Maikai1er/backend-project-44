8b27cf26300db067cd390d7833fee762409edde3
Maikai1er <kolesnichenko.misha12@gmail.com>
2022-12-29 17:59:29 +0100

    fixing linter errors

--- src/getRandomNumberFromInterval.js
+++ src/getRandomNumberFromInterval.js
@@ -1,5 +1,3 @@
-const getRandomNumber = (min = 0, max = 50) => {
-  return Math.floor(Math.random() * (max + 1 - min) + min);
-};
+const getRandomNumber = (min = 0, max = 50) => Math.floor(Math.random() * (max + 1 - min) + min);
 
 export default getRandomNumber;
